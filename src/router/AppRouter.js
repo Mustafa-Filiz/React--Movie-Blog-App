fe2262c6fafb10eb104e5be9075424efca9b357a
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-10-07 01:59:21 +0300

    main page added

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import NavBar from '../components/NavBar';
+import Main from '../pages/Main';
 
 const AppRouter = () => {
 	return (
 		<Router>
 			<NavBar />
+			<Switch>
+				<Route exact path="/" component={Main} />
+			</Switch>
 		</Router>
 	)
 }
