ebf90007c31b5c3c8a7a51fd9a079b7050785f9e
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-10-06 20:00:23 +0300

    Auth context and app router added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import AuthContextProvider from './contexts/AuthContext';
+import AppRouter from './router/AppRouter';
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <AuthContextProvider>
+        <AppRouter />
+      </AuthContextProvider>
     </div>
-  );
+  );  
 }
 
 export default App;
