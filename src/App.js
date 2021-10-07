20c8fbdfea7149abce6dc89074cdcbc255a7722c
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-10-07 13:27:53 +0300

    search context added to app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import './App.css';
 import AuthContextProvider from './contexts/AuthContext';
+import SearchContextProvider from './contexts/SearchContext';
 import AppRouter from './router/AppRouter';
 
 function App() {
-  return (
-    <div className="App">
-      <AuthContextProvider>
-        <AppRouter />
-      </AuthContextProvider>
-    </div>
-  );  
+    return (
+        <div className="App">
+            <AuthContextProvider>
+                <SearchContextProvider>
+                    <AppRouter />
+                </SearchContextProvider>
+            </AuthContextProvider>
+        </div>
+    );
 }
 
 export default App;
