7c6f9a42a27a5a2f6b8465238e067937d647024c
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-10-07 18:23:50 +0300

    register added

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import NavBar from '../components/NavBar';
+import Login from '../pages/Login';
 import Main from '../pages/Main';
+import Register from '../pages/Register';
 
 const AppRouter = () => {
 	return (
@@ -9,6 +11,8 @@ const AppRouter = () => {
 			<NavBar />
 			<Switch>
 				<Route exact path="/" component={Main} />
+				<Route path="/Login" component={Login} />
+				<Route path="/register" component={Register} />
 			</Switch>
 		</Router>
 	)
