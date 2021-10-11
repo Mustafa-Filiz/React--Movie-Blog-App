1a0bb7cc4e7642dc6542e3083a5f51edf954fe75
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-10-11 17:41:16 +0300

    context modified

--- src/contexts/SearchContext.js
+++ src/contexts/SearchContext.js
@@ -1,23 +1,21 @@
-import { createContext, useState, useEffect, useContext } from "react";
+import { createContext, useState, useEffect, useContext } from 'react';
 import axios from 'axios';
-import { AuthContext } from "./AuthContext";
+import { AuthContext } from './AuthContext';
 
 const FEATURED_URL =
     'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=c5fd6eb749e7402b687b8c200a3472c7';
 
 const SEARCH_URL =
     'https://api.themoviedb.org/3/search/movie?api_key=c5fd6eb749e7402b687b8c200a3472c7&query=';
- 
-
 
 export const SearchContext = createContext();
 
-function SearchContextProvider ({children}) {
-	const [movies, setMovies] = useState([]);
+function SearchContextProvider({ children }) {
+    const [movies, setMovies] = useState([]);
     const [searchTerm, setSearchTerm] = useState('');
-	const { currentUser } = useContext(AuthContext);
+    const { currentUser } = useContext(AuthContext);
 
-	const getMovies = (api) => {
+    const getMovies = (api) => {
         axios.get(api).then((res) => setMovies(res.data.results));
     };
 
@@ -40,15 +38,18 @@ function SearchContextProvider ({children}) {
         }
     };
 
-	const values = {
-		movies, searchTerm, handleChange, handleSearch
-	}
-
-	return(
-		<SearchContext.Provider value={values} >
-			{children}
-		</SearchContext.Provider>
-	)
+    return (
+        <SearchContext.Provider
+            value={{
+                movies,
+                searchTerm,
+                handleChange,
+                handleSearch,
+            }}
+        >
+            {children}
+        </SearchContext.Provider>
+    );
 }
 
 export default SearchContextProvider;
