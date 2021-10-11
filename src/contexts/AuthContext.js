0a4de1280b76810d77140b6bd66282815485368d
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-10-11 22:30:03 +0300

    auth context modified

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,9 +1,14 @@
 import { createContext, useState, useEffect } from 'react';
+import { observer } from '../auth/Firebase';
 
 export const AuthContext = createContext();
 
 function AuthContextProvider({ children }) {
     const [currentUser, setCurrentUser] = useState();
+
+    useEffect(() => {
+        observer(setCurrentUser)
+    }, [])
 
     return (
         <AuthContext.Provider value={{ currentUser }}>
