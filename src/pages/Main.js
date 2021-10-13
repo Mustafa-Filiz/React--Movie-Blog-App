25c359875ebf80ecf040d20d31779e1ad299b927
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-10-13 11:28:45 +0300

    last commit

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,37 +1,10 @@
 import { Grid } from '@mui/material';
-import React, { useState, useEffect, useContext } from 'react';
+import React, { useContext } from 'react';
 import MovieCard from '../components/MovieCard';
-import { AuthContext } from '../contexts/AuthContext';
 import { SearchContext } from '../contexts/SearchContext';
 
 const Main = () => {
-    // const [movies, setMovies] = useState([]);
-    // const [searchTerm, setSearchTerm] = useState('');
-    // const { currentUser } = useContext(AuthContext);
     const { movies } = useContext(SearchContext);
-
-    // const getMovies = (api) => {
-    //     axios.get(api).then((res) => setMovies(res.data.results));
-    // };
-
-    // useEffect(() => {
-    //     getMovies(FEATURED_API);
-    // }, []);
-
-    // const handleChange = (e) => {
-    //     setSearchTerm(e.target.value);
-    // };
-
-    // const handleSearch = (e) => {
-    //     e.preventDefault();
-
-    //     if (searchTerm && currentUser) {
-    //         getMovies(SEARCH_API + searchTerm);
-    //         setSearchTerm('');
-    //     } else {
-    //         alert('Please login to search a movie.');
-    //     }
-    // };
 
     return (
         <Grid
